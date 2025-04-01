db74868b3f307181221a5486a7091b60edbbb1bc
Aman Pandey <skytechno00@gmail.com>
2025-04-01 18:33:10 +0530

    seo done

--- src/views/SeoProjectView/index.tsx
+++ src/views/SeoProjectView/index.tsx
@@ -1,10 +1,10 @@
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, } from 'react';
 import styles from "./SeoProjectView.module.css"
 import Sidebar from "../../components/common/Sidebar";
 import Timeline from "../../components/common/Timeline";
-import useFetchProjects from '../../utilities/customHooks/useFetchProjects';
-import { dividerClasses } from '@mui/material';
+import useProjectView from '../ProjectView/useProjectView';
+
 const SeoProjectView = () => {
     const scrollContainerRef = useRef<HTMLDivElement>(null);
     const handleWheel = (e: React.WheelEvent) => {
@@ -13,82 +13,29 @@ const SeoProjectView = () => {
             scrollContainerRef.current.scrollLeft += e.deltaY;
         }
     };
-    const [timelinedata, setTimelinedata] = useState([
-        "Keyword Research",
-        "Competitor Analysis",
-        "On-Page Optimization",
-        "Content Creation",
-        "Technical SEO",
-        "Link Building",
-        "Performance Monitoring",
-        "Continuous Optimization"
-    ])
-    const [projects, setProjects] = useState({
-        "ios": [
-            { image: "/images/project1.png", },
-            { image: "/images/project2.png", },
-            { image: "/images/project3.png", },
-            { image: "/images/project4.png", },
-        ],
-        "android": [
-            { image: "/images/project2.png", },
-            { image: "/images/project3.png", },
-            { image: "/images/project4.png", },
-            { image: "/images/project1.png", },
-        ],
-        "web": [
-            { image: "/images/project3.png", },
-            { image: "/images/project4.png", },
-            { image: "/images/project1.png", },
-            { image: "/images/project2.png", },
-        ],
-    });
-    // const { loading, data, error } = useFetchProjects("seo");
-    // const { title, section_heading, projects: projectsApi, timeline } = data || {};
-    // console.log(data);
-    // const loading = true;
 
-    const [links, setLink] = useState([
-        { label: "IOS App", value: "ios", },
-        { label: "Android App", value: "android", },
-        { label: "Website | Portal", value: "web", },
-        { label: "IOS App", value: "ios", },
-        { label: "Android App", value: "android", },
-        { label: "Website | Portal", value: "web", },
-        { label: "IOS App", value: "ios", },
-        { label: "Android App", value: "android", },
-        { label: "Website | Portal", value: "web", },
-        { label: "IOS App", value: "ios", },
-        { label: "Android App", value: "android", },
-        { label: "Website | Portal", value: "web", },
-    ])
+    const { title, error, subTitle, timelinedata, setSelectedType, links, loading, currentProject } = useProjectView("seo");
 
-    const [selectedType, setSelectedType] = useState<"ios" | "android" | "web">("ios");
-    const handleLinkClick = (value: "ios" | "android" | "web") => {
+    const handleLinkClick = (value: string) => {
         setSelectedType(value);
         if (scrollContainerRef.current) {
             scrollContainerRef.current.scrollTo({ left: 0, behavior: "smooth" });
         }
     };
-    const currentProject = projects[selectedType];
-    // if (loading)
-    //     return (
-    //         <div className="flex space-x-10 justify-center">
-    //             <div className="w-full sm:w-[48%] lg:w-[30%] h-[450px] bg-black animate-pulse rounded-lg"></div>
-    //             <div className="w-full sm:w-[48%] lg:w-[30%] h-[450px] bg-black animate-pulse rounded-lg"></div>
-    //             <div className="w-full sm:w-[48%] lg:w-[30%] h-[450px] bg-black animate-pulse rounded-lg"></div>
-    //         </div>)
+
+    if (loading) return <div>Loading...</div>
+    if (error) return null
     return (
         <>
             <div className={`flex flex-col space-x-4 mx-auto ${styles.mainContainer}`}>
                 <div className={`flex space-x-4 ${styles.headingContainer}`} >
                     <div className={`${styles.emptyDiv}`}></div>
-                    <h2 className={`${styles.mainHeading}`}>SEO</h2>
+                    <h2 className={`${styles.mainHeading}`}>{title}</h2>
                 </div>
                 <div className={`flex space-x-4 mx-auto ${styles.contentContainer}`}>
                     <Sidebar handleClick={handleLinkClick} links={links} />
                     <div className={`px-5 py-2 flex-1 trans-black-bg scrollbar-hidden ${styles.projectsContainer}`}>
-                        <h2 className={`text-2xl mb-2 ${styles.sectionHeading}`} >Project Summary</h2>
+                        <h2 className={`text-2xl mb-2 ${styles.sectionHeading}`} >{subTitle}</h2>
                         <div
                             onWheel={handleWheel}
                             ref={scrollContainerRef}
