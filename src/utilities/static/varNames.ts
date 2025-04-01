393b44b1bbb470136740615544d2f3778d54ab3b
Aman kumar Pandey <137035478+iamanpandey11@users.noreply.github.com>
2025-04-02 00:18:13 +0530

    done

--- src/utilities/static/varNames.ts
+++ src/utilities/static/varNames.ts
@@ -1,10 +1,7 @@
-type Environment = "dev" | "prod";
-let MODE: Environment = "dev";
-
 export type Endpoint = "seo" | "development" | "testimonial";
-export let BASE_URL: string;
-if (MODE === "dev") {
-  BASE_URL = "http://markprodenv.eba-kpftbw52.ap-south-1.elasticbeanstalk.com/";
-} else if (MODE === "prod") {
-  BASE_URL = "https://domainapi.store/";
-}
+export let BASE_URL: string = "https://marvellapi.store/";
+// if (MODE === "dev") {
+//   BASE_URL = "http://markprodenv.eba-kpftbw52.ap-south-1.elasticbeanstalk.com/";
+// } else if (MODE === "prod") {
+//   BASE_URL = "https://marvellapi.store/";
+// }
