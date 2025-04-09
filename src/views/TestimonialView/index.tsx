2427cf0f3da60544b319e4642147cb2a56c31ec5
Aman Pandey <skytechno00@gmail.com>
2025-04-09 12:41:51 +0530

    some changes done

--- src/views/TestimonialView/index.tsx
+++ src/views/TestimonialView/index.tsx
@@ -66,55 +66,7 @@ const TestimonialView = () => {
           ))}
         </div>
       </div>
-
     </div>
-    // <div className={` ${styles.mainContainer}`}>
-    //   <div className={`flex space-x-4 ${styles.headingContainer}`}>
-    //     <div className={`${styles.emptyDiv}`}></div>
-    //     <h2 className={`${styles.mainHeading}`}>{title}</h2>
-    //   </div>
-    //   <div className={`flex space-x-4 mx-auto ${styles.contentContainer}`}>
-    //     <Sidebar handleClick={handleClick} links={links} />
-    //     <div
-    //       className={`px-5 py-2 flex-1 trans-black-bg scrollbar-hidden ${styles.projectsContainer}`}
-    //     >
-    //       <div className={`flex justify-between`}>
-    //         <div className={`${styles.userContainer}`}>
-    //           <img
-    //             className={` ${styles.userImage} `}
-    //             src={client_image}
-    //             alt=""
-    //           />
-    //           <h2 className={`text-2xl mb-2 ${styles.sectionHeading}`}>
-    //             {client_name} | {client_segment}
-    //           </h2>
-    //         </div>
-    //         <div className={`${styles.ratingContainer}`}>
-    //           {[...Array(stars)].map((_, index) => (
-    //             <span key={index}>⭐</span>
-    //           ))}
-    //         </div>
-    //         <div className={`${styles.recIconContainer}`}>
-    //           <img src="/images/recicon.png" alt="" />
-    //         </div>
-    //       </div>
-    //       <div
-    //         onWheel={handleWheel}
-    //         ref={scrollContainerRef}
-    //         className={`flex flex-nowrap overflow-auto ${styles.testimonialImageContainer} ${styles.midContainer}`}
-    //       >
-    //         {images.map((image, index) => (
-    //           <img
-    //             key={index}
-    //             className={`${styles.testimonialImage} ${styles.image}`}
-    //             src={image}
-    //             alt=""
-    //           />
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
