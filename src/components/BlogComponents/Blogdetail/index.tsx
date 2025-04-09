6cb1e1361409b83bbcfe939d2bce3b09836e27b0
Aman Pandey <skytechno00@gmail.com>
2025-04-09 14:21:19 +0530

    Everything done

--- src/components/BlogComponents/Blogdetail/index.tsx
+++ src/components/BlogComponents/Blogdetail/index.tsx
@@ -4,20 +4,20 @@ import parse from "html-react-parser"
 const Blogdetail = ({ blog }: { blog: BlogDetailType | null }) => {
     if (!blog) return null
     return (
-        <div className={`trans-black-bg scrollbar-hidden ${styles.mainContainer} `}>
-            <div className="flex flex-col justify-center items-center gap-4">
+        <div className={`trans-black-bg scrollbar-hidden ${styles.mainContainer}`}>
+            <div className={`${styles.blogMetaContainer}`}>
                 <h1 className={`${styles.blogTitle}`}>{blog?.title}</h1>
-                <div className="flex justify-center items-center gap-4">
-                    <div className={`flex gap-2 ${styles.authorContainer}`}>
+                <div className={`${styles.blogMeta}`}>
+                    <div className={`flex  ${styles.authorContainer}`}>
                         <img
                             className="h-6"
                             src={blog?.author_image}
-                            alt=""
+                            alt={blog?.author_name}
                         />
-                        <p>{blog?.author_name}</p>
+                        <p className={`${styles.ellipsis}`}>{blog?.author_name}</p>
                     </div>
-                    <p>{blog?.date}</p>
-                    <p>{blog?.read_min}</p>
+                    <p className={`${styles.ellipsis}`}>{blog?.date}</p>
+                    <p className={`${styles.ellipsis}`}>{blog?.read_min}</p>
                 </div>
             </div>
             <div className={`${styles.blogContentContainer} `}>
