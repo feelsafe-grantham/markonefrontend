7f49c2bf536e58e8ebc9042d2b5b8c0cd38bd081
Aman Pandey <skytechno00@gmail.com>
2025-04-09 16:45:15 +0530

    swiper js done

--- src/components/common/Carousel/index.tsx
+++ src/components/common/Carousel/index.tsx
@@ -14,6 +14,9 @@ const Carousel = () => {
                     loop={true}
                     slidesPerView={3}
                     spaceBetween={10}
+                    autoplay={true}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
                     coverflowEffect={{
                         rotate: 0,
                         stretch: 0,
@@ -21,15 +24,52 @@ const Carousel = () => {
                         modifier: 2.5,
                         slideShadows: true,
                     }}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                    className="mySwiper"
                 >
-                    <SwiperSlide className={styles.content}>
+                    <SwiperSlide >
+                        <div className={styles.content}>
+                            <img className={styles.image} src="/images/vartical1.png" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className={`${styles.content}`}>
+
+                            <img className={styles.image} src="/images/vartical2.png" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className={styles.content}>
+
+                            <img className={styles.image} src="/images/vartical3.png" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className={styles.content}>
+
+                            <img className={styles.image} src="/images/vartical4.png" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className={styles.content}>
+                            <img className={styles.image} src="/images/vartical1.png" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <div className={styles.content}>
+                            <img className={styles.image} src="/images/image6.jpg" alt="Photography 1" />
+                            <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
+                        </div>
+                    </SwiperSlide>
+                    {/* <SwiperSlide className={styles.content}>
                         <img className={styles.image} src="/images/image4.jpg" alt="Photography 1" />
                         <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
                     </SwiperSlide>
@@ -58,11 +98,9 @@ const Carousel = () => {
 
                     </SwiperSlide>
                     <SwiperSlide className={styles.content}>
-
                         <img className={styles.image} src="/images/image6.jpg" alt="Photography 1" />
                         <button className={`${styles.btn} ${styles.downloadbtn}`}>Download</button>
-
-                    </SwiperSlide>
+                    </SwiperSlide> */}
                 </Swiper>
             </div>
         </section>
